d2248baa23dfc0e93eb8740a362919da68cc437e
Ross <r0554613@gmail.com>
2013-12-25 18:02:51 -0800

    moving platforms?

--- Broccolaser/Camera.cpp
+++ Broccolaser/Camera.cpp
@@ -7,6 +7,7 @@ Camera::Camera(Player* player, View* view)
 	this->player = player;
 	this->view = view;
 	this->position = player->position;
+	view->zoom(0.5f);
 }
 
 void Camera::update()
@@ -14,10 +15,8 @@ void Camera::update()
 	Vector2f target(player->position.x + player->boundingBox.width/2, player->position.y + player->boundingBox.height/2);
 	Vector2f velocity(target.x - position.x, target.y - position.y);
 	velocity /= 5.0f;
-	
-	//view->setRotation(position.y); //OH GOD
-	
+		
 	position += velocity;
 	
-	view->setCenter(position);
+	view->setCenter(640,400);
 }
